14116c889220aebe43d45348e4ad78f4b4f2dfa2
JamesJJT <38963376+JamesJJT@users.noreply.github.com>
2023-02-28 16:58:49 +0000

    fix php stan issues

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index($int)
+    public function index(Int $int): Int
     {
-
+        return $int;
     }
 }
